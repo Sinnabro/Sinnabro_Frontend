6cbefe38dd48bc91e61b7089437b1f052f7e0d30
kimgyuha <kimgyuha25@gmail.com>
2022-11-22 22:44:01 +0900

    Update Router MainPage

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import BeforeLogin from "./pages/BeforeLogin";
+import Main from "./pages/Main";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<BeforeLogin/>} />
+        <Route path="/main" element={ <Main/> } />
       </Routes>
     </BrowserRouter>
   );
