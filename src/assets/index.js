fba848caf2478d199dd134192d2b1b177cf4fa4e
kimgyuha <kimgyuha25@gmail.com>
2022-11-22 19:55:31 +0900

    Create Assets Index

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+export { default as Computer } from "./img/ComputerImg.png";
+export { default as FaceBookImg } from "./img/FaceBookImg.png";
+export { default as GithubImg } from "./img/GithubImg.png";
+export { default as Logo } from "./img/LogoImg.png";
+export { default as MeetingImg } from "./img/MeetingImg.png";
+export { default as TodoImg } from "./img/TodoImg.png";
