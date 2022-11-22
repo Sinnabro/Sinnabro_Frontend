b9c1bd5a8790a676de6b5a8551fa9cd1449623a3
kimgyuha <kimgyuha25@gmail.com>
2022-11-22 19:56:10 +0900

    Update Router BeforeLogin

--- src/Router.js
+++ src/Router.js
@@ -1,9 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import BeforeLogin from "./pages/BeforeLogin";
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<BeforeLogin/>} />
+      </Routes>
     </BrowserRouter>
   );
 };
