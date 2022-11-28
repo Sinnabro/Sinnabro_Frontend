52a4d7195107cde03b71601b665d2e3dec7833f9
SEOg-u <seogyeoul1898@gmail.com>
2022-11-28 22:57:15 +0900

    Update Router

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import BeforeLogin from "./pages/BeforeLogin";
 import Mypage from "./pages/MyPage";
+import SignUp from "./pages/SignUp";
+import LogIn from "./pages/LogIn";
+import RevisionPw from "./pages/RevisionPw";
+import FindPw from "./pages/FindPw";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<BeforeLogin/>} />
-        <Route path="/mypage" element={ <Mypage/> } />
+        <Route path="/" element={<BeforeLogin />} />
+        <Route path="/mypage" element={<Mypage />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/revisionpw" element={<RevisionPw />} />
+        <Route path="/findpw" element={<FindPw />} />
       </Routes>
     </BrowserRouter>
   );
