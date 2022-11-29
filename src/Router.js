8b49c0c7879063e9caf89a5d44f8dd2d26ce9635
kimgyuha <kimgyuha25@gmail.com>
2022-11-29 23:01:29 +0900

    Update Router DayStudent

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import BeforeLogin from "./pages/BeforeLogin";
 import Mypage from "./pages/MyPage";
+import DayUser from "./pages/DayUser";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<BeforeLogin/>} />
-        <Route path="/mypage" element={ <Mypage/> } />
+        <Route path="/user" element={<DayUser />} />
+        <Route path="/" element={<BeforeLogin />} />
+        <Route path="/mypage" element={<Mypage />} />
       </Routes>
     </BrowserRouter>
   );
