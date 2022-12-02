6d87e1d0ee39d6793ab5daf735af3500cc2a01d8
kimgyuha <kimgyuha25@gmail.com>
2022-12-02 18:54:00 +0900

    Update MainPage

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,10 +1,25 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import DDay from "../components/DDay";
-import Calendar from "../components/main/Calendar";
+import LeftCalendar from "../components/main/Calendar/LeftCalendar";
+// import Calendar from "../components/main/Calendar--";
 import VeiwTodo from "../components/main/VeiwTodo";
 
 const Main = () => {
+  const [day, setDay] = useState(new Date());
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const month = day.getMonth() + 1;
+    const date = day.getDate();
+    if (count) {
+      console.log(month, date);
+    } else {
+      setCount(1);
+    }
+  }, [day]);
+
   let TodoData = [
     {
       todo_id: 1,
@@ -47,23 +62,20 @@ const Main = () => {
       createdAt: "2022-10-06",
     },
   ];
-  let DDayData = {
-    dayname: "기말고사",
-    date: "2022-12-21",
-  };
 
   return (
     <>
       <All>
         <Left>
           <Go to="/">
-            <DDay data={DDayData} />
+            <DDay height="200px" />
           </Go>
           <Go to="/">
             <VeiwTodo data={TodoData} />
           </Go>
         </Left>
-        <Calendar />
+        {/* <Calendar /> */}
+        <LeftCalendar day={day} setDay={setDay} />
       </All>
     </>
   );
