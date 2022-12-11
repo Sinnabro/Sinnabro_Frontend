1b41410325548011f9f453deb8e7061e8d5dbdd8
SEOg-u <seogyeoul1898@gmail.com>
2022-12-12 08:39:12 +0900

    Update Error

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -3,22 +3,23 @@ import styled from "styled-components";
 import { Error404 } from "../assets";
 
 const NotFound = () => {
-  return (
-    <Wrapper>
-      <Div src={Error404} />
-    </Wrapper>
-  );
+  return <Wrapper>{/* <Div src={Error404} /> */}</Wrapper>;
 };
-const Div = styled.img`
-  margin-top: 90px;
-  width: 70%;
-  margin-left: 5%;
-`;
+// const Div = styled.img`
+//   margin-top: 90px;
+//   width: 70%;
+//   margin-left: 5%;
+// `;
 const Wrapper = styled.div`
   width: 100vw;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding-top: 160px;
+  height: 100vh;
+  /* display: flex; */
+  /* align-items: center; */
+  /* justify-content: center; */
+  /* padding-top: 160px; */
+  background-image: url(${Error404});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 40%;
 `;
 export default NotFound;
