46f25a8fce4f2332a4d1ef8331a935d5fb843a3b
SEOg-u <seogyeoul1898@gmail.com>
2022-12-12 19:17:14 +0900

    Update Router

--- src/Router.js
+++ src/Router.js
@@ -3,10 +3,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import BeforeLogin from "./pages/BeforeLogin";
 import Mypage from "./pages/MyPage";
 import DayUser from "./pages/DayUser";
-// import SignUp from "./pages/SignUp";
-// import LogIn from "./pages/LogIn";
-// import RevisionPw from "./pages/RevisionPw";
-// import FindPw from "./pages/FindPw";
+import SignUp from "./pages/auth/SignUp";
+import LogIn from "./pages/auth/LogIn";
+import RevisionPw from "./pages/auth/RevisionPw";
+import FindPw from "./pages/auth/FindPw";
 import Main from "./pages/Main";
 import Header from "./components/Header";
 import ViewMain from "./pages/ViewMain";
@@ -21,10 +21,10 @@ const Router = () => {
           <Route path="/" element={<BeforeLogin />} />
           <Route path="/main" element={<Main />} />
           <Route path="/mypage" element={<Mypage />} />
-          {/* <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<LogIn />} />
-        <Route path="/revisionpw" element={<RevisionPw />} />
-      <Route path="/findpw" element={<FindPw />} /> */}
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/revisionpw" element={<RevisionPw />} />
+          <Route path="/findpw" element={<FindPw />} />
           <Route path="/view" element={<ViewMain />} />
         </Route>
         <Route path="*" element={<NotFound />} />
